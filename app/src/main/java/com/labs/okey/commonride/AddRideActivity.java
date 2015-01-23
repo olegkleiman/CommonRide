6beef07909650a86035302976bccc621987c8305
olegkleiman <oleg_kleyman@hotmail.com>
2015-01-23 03:58:17 +0200

    base version

---
+++ app/src/main/java/com/labs/okey/commonride/AddRideActivity.java
@@ -0,0 +1,86 @@
+package com.labs.okey.commonride;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.labs.okey.commonride.model.Ride;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.TableOperationCallback;
+
+import java.util.Date;
+
+
+public class AddRideActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = "CommonRide.AddRide";
+
+    private MobileServiceClient mClient;
+    private MobileServiceTable<Ride> mRidesTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ride);
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://commonride.azure-mobile.net/",
+                    "RuDCJTbpVcpeCQPvrcYeHzpnLyikPo70",
+                    this);
+            mRidesTable = mClient.getTable("commonrides", Ride.class);
+        } catch(Exception e) {
+            Log.i(LOG_TAG, e.getMessage());
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_ride, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void btnAddRideClick(View v) {
+
+        Ride ride = new Ride();
+        ride.whenPublished = new Date();
+        ride.whenStarts = new Date();
+        ride.driver = "fb:555";
+        ride.from = "Milano";
+        ride.to = "Firenze";
+        ride.freePlaces = 3;
+
+        mRidesTable.insert(ride, new TableOperationCallback<Ride>() {
+            public void onCompleted(Ride entity,
+                                    Exception exception,
+                                    ServiceFilterResponse response) {
+                if (exception == null) {
+                    Log.i(LOG_TAG, "Inserted object with ID " + entity.Id);
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/labs/okey/commonride/model/Join.java
@@ -0,0 +1,13 @@
+package com.labs.okey.commonride.model;
+
+/**
+ * Created by Oleg on 23-Jan-15.
+ */
+public class Join {
+    @com.google.gson.annotations.SerializedName("id")
+    public String Id;
+
+    @com.google.gson.annotations.SerializedName("ride_id")
+    public String rideId;
+
+}
